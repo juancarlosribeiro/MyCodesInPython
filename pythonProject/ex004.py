b8427be8846315528d3d3a2e92af4122cd9b9585
Juan Carlos Ribeiro Fiuza <juancarlosribeiro167@gmail.com>
2024-06-11 17:51:35 -0300

    Upload2

---
+++ pythonProject/ex004.py
@@ -0,0 +1,13 @@
+thevar = input('Digite algo: ')
+print('O tipo primitivo desse valor é:', type(thevar))
+print('Só tem espacos? ', thevar.isspace())
+print('É um numero? ', thevar.isnumeric())
+print('É alfabetico? ', thevar.isalpha())
+print('É alfanúmerico? ', thevar.isalnum())
+print('Está em maiúsculas? ', thevar.isupper())
+print('Está em minúsculas? ', thevar.islower())
+print('Está capitalizada?', thevar.istitle())
+
+
+
+
